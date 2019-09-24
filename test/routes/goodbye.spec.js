3db4849d96b858977da4ea2a2a416d454911428e
Seth Kress <kresss@users.noreply.github.com>
2019-09-24 17:23:26 -0400

    Create a goodbye api

---
+++ test/routes/goodbye.spec.js
@@ -0,0 +1,16 @@
+const server = require('../../bin/www');
+const request = require('supertest');
+
+describe.skip('Goodbye API', function () {
+  it('Goodbye Responds with Goodbye', function (done) {
+    request(server)
+      .get('/goodbye')
+      .expect(200, 'Goodbye!', done);
+  });
+
+  it('Goodbye Responds with Goodbye Seth when given a parameter', function (done) {
+    request(server)
+      .get('/goodbye/Seth')
+      .expect(200, 'Goodbye Seth', done);
+  });
+});
