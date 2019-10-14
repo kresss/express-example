68e80a495fcc78bc26207e82b87f6a3d684040b8
kresss <kresss@gmail.com>
2019-10-14 08:30:25 -0400

    Comment out goodbye.

--- routes/goodbye.js
+++ routes/goodbye.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+/*
 router.get('/', function (req, res, next) {
   res.send("Goodbye!");
 });
@@ -9,5 +10,5 @@ router.get('/', function (req, res, next) {
 router.get('/:name', function (req, res, next) {
   res.send('Goodbye ' + req.params.name + '!');
 });
-
+*/
 module.exports = router;
