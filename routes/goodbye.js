49aac5a83afa2625d67e2651c01b238a753403a7
Seth Kress <kresss@users.noreply.github.com>
2019-10-25 07:37:04 -0400

    Revert "Create goodbye api Issue #3 (#11)"
    This reverts commit 7549a75b30768b30e761637c40bf341c1056888b.

--- routes/goodbye.js
+++ routes/goodbye.js
@@ -2,12 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 /* GET users listing. */
+/*
 router.get('/', function (req, res, next) {
-  res.send('Goodbye!');
+  res.send("Goodbye!");
 });
 
 router.get('/:name', function (req, res, next) {
   res.send('Goodbye ' + req.params.name + '!');
 });
-
+*/
 module.exports = router;
